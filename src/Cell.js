f82f26ecfeedcde2c357d86d5d306705409cef7f
Alicia Weenum <weenum.alicia@gmail.com>
2021-02-25 20:44:25 -0500

    commit tree 9a2080ef7ee7680c0bf9e74415ce6ea9b261db72

---
+++ src/Cell.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+export default class Cell extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      color: props.value
+    } // ...define initial state with a key of 'color' set to the 'value' prop
+  } 
+
+  someFunction = () => {
+    this.setState({
+      color: "#333"
+    })
+  }
+
+
+    render() {
+        return (
+          
+          <div className="cell" style={{backgroundColor: this.state.color}} onClick={this.someFunction}> 
+          {this.state.color}
+          
+     
+         
+           
+          </div>
+        )
+      }
+}
